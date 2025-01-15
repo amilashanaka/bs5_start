ddbcd6bbfafd2f745dcb5b921cdcc3626a5ca44a
amilashanaka <dsa.amilashanaka@gmail.com>
2025-01-15 13:29:36 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet"  crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+</head>
+
+<body>
+    <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="https://placehold.co/600x400" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="https://placehold.co/600x400" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="https://placehold.co/600x400" class="d-block w-100" alt="...">
+            </div>
+        </div>
+    </div>
+   
+    <script src="assets/js/bootstrap.bundle.min.js"   crossorigin="anonymous"></script>
+
+</body>
+
+</html>
